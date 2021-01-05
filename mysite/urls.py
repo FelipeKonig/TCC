8f26679286ef7814a4181aad986c83e995d979a2
FelipeKonig <lipekonig@gmail.com>
2021-01-05 15:38:40 -0300

    acesso para admin alterar usuarios adicionado

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,9 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-                  path('', include('ecommerce.urls')),
-                  path('admin/', admin.site.urls),
-                  path('usuarios/', include('usuarios.urls')),
-                  path('accounts/', include('django.contrib.auth.urls')),
-
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('ecommerce.urls')),
+    path('admin/', admin.site.urls),
+    path('usuarios/', include('usuarios.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
